5cdaa6a17fdb1dacb77911697570a8e2fb7c4b12
Samarth3707 <93036928+Samarth3707@users.noreply.github.com>
2021-11-15 12:31:22 +0530

    Add files via upload

---
+++ Evennumber.java
@@ -0,0 +1,15 @@
+package Evennumber;
+
+public class Evennumber {
+	public static void main(String[] args) {
+		int num=5;
+		for(int i=1;i<=10;i++)
+			System.out.println(num+"*"+i+"="+num*i);
+			
+		
+		
+		}
+			
+	}
+
+
